f436fe933e26b2e9abf582c1aaf6a587cda93d60
Gaurav Manek <gaurav@gauravmanek.com>
2014-07-31 15:14:26 +0800

    Implemented with bytearray

--- generate.py
+++ generate.py
@@ -8,10 +8,37 @@ imgWidth = 816
 
 def main():
 	(sz, bimg) = loadImage("test.jpg")
+	bdata = convertToBinary(sz, bimg)
 	
 
 # Convert input image to compliant binary image:
-#def convertToBinary((wd, ht), img):
+def convertToBinary((wd, ht), img):
+	rv = bytearray()
+	rowsPerLine = 8	 # rows of image data per line of printer data
+
+	for lineStartNum in range(0, ht, rowsPerLine): # For each block of rows that make a line
+		# Emit the appropriate headers:
+		rv.append(27) 				# Set image mode
+		rv.append(ord('K'))			# Single density
+		rv.append(imgWidth % 256) 	# Low byte of image width
+		rv.append(imgWidth // 256) 	# High byte of image width
+
+		for i in range(wd): # For each column of pixels
+			colVal = 0 # The value that encodes all the states in this column
+			cPin = 0b10000000 # The moving filter
+			for j in range(lineStartNum, min(lineNum + rowsPerLine, ht)):
+				if img[i,j] == 0: # If this pin should be triggered.
+					colVal |= cPin
+				cPin >>= 1
+			rv.append(colVal) # Add the column to the output
+
+		# We're done with this line, emit a newline character:
+		rv.append(ord('\r')) # Yes, the printer is that old.
+		rv.append(ord('\n'))
+
+	for i in range(4): # Trailing newlines:
+		rv.append(ord('\r')) # Yes, the printer is that old.
+		rv.append(ord('\n'))
 
 
 def loadImage(fname):
@@ -41,8 +68,8 @@ def loadImage(fname):
 
 	img_resized.show()
 
-	return ((wd, ht), [[True if pix[i,j] == 0 else False for i in range(wd)] for j in range(ht)])
-	# return ((wd, ht), pix)
+	#return ((wd, ht), [[True if pix[i,j] == 0 else False for i in range(wd)] for j in range(ht)])
+	return ((wd, ht), pix)
 
 
 if __name__ == "__main__":
